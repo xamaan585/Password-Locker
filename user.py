71b56b8fd482bcc5ce378b56af89cadd0e0c5c15
Abdirahman <dubowhamaan@gmail.com>
2022-01-21 14:13:38 +0300

    adding user.py

--- user.py
+++ user.py
@@ -8,3 +8,18 @@ class User:
     def __init__(self, username,password):
         self.username = username
         self.password = password
+
+        def save_user(self):
+            '''
+            method to login user object to user list
+            '''
+            User.user_list.append(self)
+
+        @classmethod
+        def user_verification(cls,username,password):
+            '''
+            method to login user credentials on login
+            '''
+            for user in User.user_list:
+                if(user.username == username and user.password == password):
+                    return user
