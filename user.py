ea5ca487687d8018ef8306d0ee7450fabe04ea25
Abdirahman <dubowhamaan@gmail.com>
2022-02-08 10:42:08 +0300

    user_test.py

--- user.py
+++ user.py
@@ -5,21 +5,21 @@ class User:
 
     user_list = []
 
-    def __init__(self, username,password):
+    def __init__(self,username,password):
         self.username = username
         self.password = password
 
-        def save_user(self):
-            '''
-            method to login user object to user list
-            '''
-            User.user_list.append(self)
+    def save_user(self):
+        '''
+        method to login user object to user list
+        '''
+        User.user_list.append(self)
 
-        @classmethod
-        def user_verification(cls,username,password):
-            '''
-            method to login user credentials on login
-            '''
-            for user in User.user_list:
-                if(user.username == username and user.password == password):
-                    return user
+    @classmethod
+    def user_verification(cls,username,password):
+        '''
+        method to login user credentials on login
+        '''
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                return user
